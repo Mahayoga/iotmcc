4e1ea0d86237fee8d9cedfa5fee16cded95d205e
abibayu <nerds2k22@gmail.com>
2025-10-08 12:22:09 +0700

    naming section

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,7 +4,6 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <meta name="description" content="">
   <meta name="author" content="">
 
@@ -12,47 +11,28 @@
 
   <!-- CSS FILES -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
-
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Open+Sans&display=swap"
-    rel="stylesheet">
-
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Open+Sans&display=swap"rel="stylesheet">
   <link href="{{ asset('assets/user/css/bootstrap.min.css') }}" rel="stylesheet">
-
   <link href="{{ asset('assets/user/css/bootstrap-icons.css') }}" rel="stylesheet">
-
   <link href="{{ asset('assets/user/css/templatemo-topic-listing.css') }}" rel="stylesheet">
-  <!--
-
-TemplateMo 590 topic listing
-
-https://templatemo.com/tm-590-topic-listing
-
--->
 </head>
 
 <body id="top">
-
   <main>
-
     <nav class="navbar navbar-expand-lg">
       <div class="container">
         <a class="navbar-brand">
           <img src="{{ asset('assets/user/images/iotmcc-icon-trans.png') }}" alt="IoTMCC Logo" width="95" height="65 "
             class="me-2">
         </a>
-
-
         <div class="d-lg-none ms-auto me-4">
           <a href="#top" class="navbar-icon bi-person smoothscroll"></a>
         </div>
-
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-lg-5 me-lg-auto">
             <li class="nav-item">
@@ -75,7 +55,6 @@ https://templatemo.com/tm-590-topic-listing
               <a class="nav-link click-scroll" href="#section_5">Lokasi</a>
             </li>
           </ul>
-
           <div class="d-none d-lg-block">
             <a href="#top" class="navbar-icon bi-person smoothscroll"></a>
           </div>
@@ -83,7 +62,7 @@ https://templatemo.com/tm-590-topic-listing
       </div>
     </nav>
 
-
+    <!-- Hero section -->
     <section class="hero-section d-flex justify-content-center align-items-center" id="section_1">
       <div class="container">
         <div class="row">
@@ -159,7 +138,7 @@ https://templatemo.com/tm-590-topic-listing
       </div>
     </section>
 
-
+    <!-- Galeri Section -->
     <section class="explore-section section-padding" id="section_2">
       <div class="container">
         <div class="col-12 text-center">
@@ -221,23 +200,20 @@ https://templatemo.com/tm-590-topic-listing
       </div>
     </section>
 
-
+    <!-- Timeline Section -->
     <section class="timeline-section section-padding" id="section_3">
       <div class="section-overlay"></div>
-
       <div class="container">
         <div class="row">
           <div class="col-12 text-center">
             <h2 class="text-white mb-4">Bagaimana IoT MCC Berjalan?</h2>
           </div>
-
           <div class="col-lg-10 col-12 mx-auto">
             <div class="timeline-container">
               <ul class="vertical-scrollable-timeline" id="vertical-scrollable-timeline">
                 <div class="list-progress">
                   <div class="inner"></div>
                 </div>
-
                 <li>
                   <h4 class="text-white mb-3">Perancangan Sistem IoT</h4>
                   <p class="text-white">
@@ -248,7 +224,6 @@ https://templatemo.com/tm-590-topic-listing
                     <i class="bi-cpu"></i>
                   </div>
                 </li>
-
                 <li>
                   <h4 class="text-white mb-3">Pengujian dan Implementasi</h4>
                   <p class="text-white">
@@ -259,7 +234,6 @@ https://templatemo.com/tm-590-topic-listing
                     <i class="bi-gear"></i>
                   </div>
                 </li>
-
                 <li>
                   <h4 class="text-white mb-3">Pemantauan dan Analisis Data</h4>
                   <p class="text-white">
@@ -277,7 +251,7 @@ https://templatemo.com/tm-590-topic-listing
       </div>
     </section>
 
-
+    <!-- faq -->
     {{-- <section class="faq-section section-padding" id="section_4">
       <div class="container">
         <div class="row">
@@ -353,6 +327,7 @@ https://templatemo.com/tm-590-topic-listing
     </section>
  --}}
 
+ <!-- lokasi section -->
     <section class="lokasi-section section-padding section-bg" id="section_5">
       <div class="container">
         <div class="row">
@@ -413,6 +388,7 @@ https://templatemo.com/tm-590-topic-listing
     </section>
   </main>
 
+  <!-- Footer section -->
   <footer class="site-footer section-padding">
     <div class="container">
       <div class="row">
