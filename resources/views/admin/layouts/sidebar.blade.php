31d0f796e0392bb9a15bb29e23f6fdb37c397fc4
Mahayoga <myoga.bahtiar@gmail.com>
2025-10-10 18:06:48 +0700

    WIP: Konsep awal dashboard

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -3,56 +3,23 @@
   <div class="sidebar-content">
     <nav class="sidebar-nav">
       <ul class="nav flex-column">
-        <li class="nav-item">
-          <a class="nav-link active" href="./index.html">
-            <i class="bi bi-speedometer2"></i>
-            <span>Dashboard</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./analytics.html">
-            <i class="bi bi-graph-up"></i>
-            <span>Tables</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./users.html">
-            <i class="bi bi-people"></i>
-            <span>Riwayat Data</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./products.html">
-            <i class="bi bi-box"></i>
-            <span>Notifications</span>
-          </a>
+        @if(Route::currentRouteName() == 'dashboard.index')
+          <li class="nav-item p-2">
+            <a class="nav-link active" href="{{ route('dashboard.index') }}">
+              <i class="bi bi-speedometer2"></i>
+              <span>Dashboard</span>
+            </a>
+          </li>
+        @else
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('dashboard.index') }}">
+              <i class="bi bi-speedometer2"></i>
+              <span>Dashboard</span>
+            </a>
+          </li>
+        @endif
         <li class="nav-item mt-3">
-          <small class="text-muted px-3 text-uppercase fw-bold">Halaman Tambah Data</small>
-        </li>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./orders.html">
-            <i class="bi bi-bag-check"></i>
-            <span>Data Kebun</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./reports.html">
-            <i class="bi bi-file-earmark-text"></i>
-            <span>Data Blok</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./calendar.html">
-            <i class="bi bi-calendar-event"></i>
-            <span>Data Pengguna</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./files.html">
-            <i class="bi bi-folder2-open"></i>
-            <span>Data Galeri</span>
-          </a>
+          <small class="text-muted px-3 text-uppercase fw-bold">--- Separator ---</small>
         </li>
       </ul>
     </nav>
