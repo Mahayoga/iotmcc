9cdf5de47c135a39f803ee9394257e58b79ed9c7
abibayu <nerds2k22@gmail.com>
2025-10-17 11:55:43 +0700

    WIP: Ruangan Pengeringan

--- app/Http/Controllers/RuanganPengeringanController.php
+++ app/Http/Controllers/RuanganPengeringanController.php
@@ -3,15 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\RuanganModel;
-use App\Models\SensorModel;
+use App\Models\GudangModel;
 use App\Models\NilaiSensorModel;
+use App\Models\SensorModel;
 
 
 class RuanganPengeringanController extends Controller
 {
      public function getDataSuhu(string $id) {
+        $dataSuhu = [];
+        $dataWaktuSuhu = [];
+        $dataKelembaban = [];
+        $dataWaktuKelembaban = [];
+        $statusRuangan = 1;
+        $dataGudang = GudangModel::findOrFail($id);
+        $dataRuangan = $dataGudang->getDataRuangan;
+        foreach ($dataRuangan as $value) { 
+            if($value->tipe_ruangan == 3) {
+                $statusRuangan = $value->status_ruangan;
+                foreach($value->getDataSensor as $value2) {
+                    if($value2->flag_sensor == 'suhu') {
+                        foreach($value2->getDataNilaiSensor()->orderBy('created_at', 'desc')->limit(30)->get() as $value3) {
+                            $dataSuhu[] = $value3->nilai_sensor;
+                            $dataWaktuSuhu[] = date('G:i:s', $value3->created_at->timestamp);
+                        }
+                    } else if($value2->flag_sensor == 'kelembaban') {
+                        foreach($value2->getDataNilaiSensor()->orderBy('created_at', 'desc')->limit(30)->get() as $value3) {
+                            $dataKelembaban[] = $value3->nilai_sensor;
+                            $dataWaktuKelembaban[] = date('G:i:s', $value3->created_at->timestamp);
+                        }
+                    }
+                }
+            }
+        }
 
+        return response()->json([
+            'status' => true,
+            'dataSuhu' => $dataSuhu,
+            'dataKelembaban' => $dataKelembaban,
+            'dataWaktuSuhu' => $dataWaktuSuhu,
+            'dataWaktuKelembaban' => $dataWaktuKelembaban,
+            'dataAvgSuhu' => number_format(array_sum($dataSuhu) / count($dataSuhu), 1),
+            'dataAvgKelembaban' => number_format(array_sum($dataKelembaban) / count($dataKelembaban), 1),
+        ]);
      }
 
     /**
