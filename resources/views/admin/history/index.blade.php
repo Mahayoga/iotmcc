7c8441b3826920797a5877019595351d18d762c5
Eka Setyo Saputra <eka@example.com>
2025-10-18 06:51:20 +0700

    update card dashboard and name user

---
+++ resources/views/admin/history/index.blade.php
@@ -0,0 +1,12 @@
+@extends('admin.layouts.main')
+@section('title', 'History')
+@section('content')
+  <div class="container-fluid px-4">
+    <h1 class="h3 text-gray-800">Maintenance</h1>
+
+    <div class="text-center my-5">
+      <h2 class="text-muted">🚧 Sedang Dalam Pengerjaan 🚧</h2>
+      <p class="text-secondary">Fitur Hiatory ini sedang kami kembangkan. Nantikan update selanjutnya!</p>
+    </div>
+  </div>
+@endsection
