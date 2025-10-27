67e0f1f8ee235f51a563badbacd97b2e0ad32b05
abibayu <nerds2k22@gmail.com>
2025-10-27 12:47:29 +0700

    done reset pasword

---
+++ app/notifications/CustomResetPassword.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class CustomResetPassword extends ResetPasswordNotification
+{
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        // Generate reset password URL
+        $url = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        return (new MailMessage)
+            ->subject('Reset Password - Breeze IoTMCC')
+            ->view('emails.reset-password', ['url' => $url]);
+    }
+}
