e9e1fd7754cf5fe1b0cb72ec8307cbcf711bbf6f
Mahayoga Ksatria Hanafi Bahtiar <144007456+Mahayoga@users.noreply.github.com>
2025-12-31 12:09:13 +0700

    Revert "Api sensor"

--- app/Http/Controllers/NilaiSensorAPIController.php
+++ app/Http/Controllers/NilaiSensorAPIController.php
@@ -58,13 +58,9 @@ class NilaiSensorAPIController extends Controller
             $flagSensor = Str::lower($dataSensor->flag_sensor ?? '');
             $nilai = (float) $request->nilai_sensor;
 
-            if($nilai < 0 || $nilai > 100){
-                $nilai = 0;
-            }
-
             NilaiSensorModel::create([
                 'id_sensor' => $request->id_sensor,
-                'nilai_sensor' => $nilai,
+                'nilai_sensor' => $request->nilai_sensor,
                 'rssi' => $request->rssi,
                 'snr' => $request->snr,
             ]);
